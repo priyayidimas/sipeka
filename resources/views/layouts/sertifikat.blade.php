acba5b27b49830fd207aecb1dbd6c959447cca3a
rifqisbgj <rifqi.subagja4@gmail.com>
2020-08-30 16:07:42 +0700

    design sertifikat

--- resources/views/layouts/sertifikat.blade.php
+++ resources/views/layouts/sertifikat.blade.php
@@ -5,22 +5,66 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"> -->
+    <!-- <link href="/assets/css/sb-admin-2.css" rel="stylesheet"> -->
 </head>
 <body>
-    <div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
-        <div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
-               <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
-               <br><br>
-               <span style="font-size:25px"><i>This is to certify that</i></span>
-               <br><br>
-               <span style="font-size:30px"><b>$student.getFullName()</b></span><br/><br/>
-               <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
-               <span style="font-size:30px">$course.getName()</span> <br/><br/>
-               <span style="font-size:20px">with score of <b>$grade.getPoints()%</b></span> <br/><br/><br/><br/>
-               <span style="font-size:25px"><i>dated</i></span><br>
-              #set ($dt = $DateFormatter.getFormattedDate($grade.getAwardDate(), "MMMM dd, yyyy"))
-              <span style="font-size:30px">$dt</span>
+    <div style="width:1122px; height:770px; text-align:center; background:url('/assets/img/bck-sertif.png');background-size:cover;">
+        <div class="content" style="display:flex;">
+            <div style="width:117px;height:118px;background:white;border-radius:50%;float:left;margin:50px 50px;box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);">
+                <img src="/assets/img/sipekawarna.png" style="margin-top:20px;margin-left:-7px;width:134px;height:89px;" alt="">
+            </div>
+            <div style="width:100%;text-align:left;">
+                <h1 style="font-family: Lucida Bright;font-style: normal;font-weight: 600;font-size: 48px;line-height: 56px;color:white;letter-spacing: 0.18em;margin-left:150px;margin-top:70px;">SERTIFIKAT</h1>
+            </div>
+        </div>
+        <div class="content" style="display:flex;">
+            <h1 style="font-family: Montserrat;
+font-style: normal;
+font-weight: bold;
+font-size: 24px;
+line-height: 29px;
+text-align: center;
+color: #FFFFFF;width:100%;">Website Apotik dengan CodeIgniter</h1>
+        </div>
+        <div class="content">
+            <div class="tujuan" style="width:100%">
+                <p style="font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+
+color: #FFFFFF;">Siswa berhasil memperoleh hasil pembelajaran yang sesuai dengan kriteria kelulusan kelas, diberikan kepada : </p>
+            </div>
+        </div>
+        <div class="content">
+            <div class="tujuan" style="width:100%">
+                <div class="box" style="padding:4px 0px; background: rgba(60, 110, 113, 0.5);">
+                    <h1 style="font-family: Montserrat;font-style: normal;font-weight: bold;font-size: 24px;line-height: 29px;text-align: center;color:white;font-variant: small-caps;">Rifqi Subagja</h1>
+                    <h3 style="font-family: Montserrat;font-style: normal;font-weight: normal;font-size: 18px;line-height: 22px;text-align: center;font-variant: small-caps;color:white;">Universitas Pendidikan Indonesia</h3>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <div class="tujuan" style="width:100%">
+                <p style="font-family: Montserrat;
+font-style: normal;
+font-weight: 500;
+font-size: 18px;
+line-height: 22px;
+text-align: center;
+
+color: #FFFFFF;">Sertifikat dibuat pada tanggal 30 Agustus 2020</p>
+            </div>
         </div>
     </div>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> -->
+    <!-- <script src="/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/sb-admin-2.js"></script> -->
 </body>
 </html>
